9f413c392aecec6b35272ad758a1b56bb70a7a74
nickgrosvenor <nick@cinegrain.com>
2015-02-19 19:07:25 -0800

    New Version

---
+++ Notes/Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging-Header.h
+//  Notes
+//
+//  Created by Nick Grosvenor on 2/19/15.
+//  Copyright (c) 2015 Nick Grosvenor. All rights reserved.
+//
+
+
+#import <Parse/Parse.h>
+
---
+++ Notes/MasterTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MasterTableViewCell.swift
+//  Notes
+//
+//  Created by Nick Grosvenor on 2/19/15.
+//  Copyright (c) 2015 Nick Grosvenor. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var masterTitleLabel: UILabel!
+    @IBOutlet weak var masterTextLabel: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
